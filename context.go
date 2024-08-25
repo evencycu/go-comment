24ec432c211022af1b71e23b96125a72bb30a9cf
even chen <even.cycu@gmail.com>
2024-08-25 23:36:21 +0800

    init version

---
+++ context.go
@@ -0,0 +1,307 @@
+package gocommont
+
+import (
+	"context"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+type RWMutexInterface interface {
+	Lock()
+	RLock()
+	RLocker() sync.Locker
+	RUnlock()
+	Unlock()
+}
+
+// Context defines interface of common usage, combing log, value, go context methods.
+type Context interface {
+	Set(key string, value interface{})
+	Get(key string) interface{}
+	GetCID() (string, error)
+	GetString(key string) (value string, ok bool)
+	GetStringSlice(key string) (value []string, ok bool)
+	GetBool(key string) (value bool, ok bool)
+	GetInt(key string) (value int, ok bool)
+	GetInt64(key string) (value int64, ok bool)
+
+	Map() (ret map[string]interface{})
+	MapString() (ret map[string]string)
+	HeaderKeyMap() (ret map[string]string)
+
+	// tracing method
+	//StartSpanFromContext(spanName string, opts ...oteltrace.SpanStartOption) oteltrace.Span
+	//GetSpan() oteltrace.Span
+	//SetSpan(span oteltrace.Span)
+	//GetBaggage() baggage.Baggage
+	//SetBaggage(baggage baggage.Baggage)
+
+	// header key transformation
+	SetShortenKey(key string, value interface{})
+	InjectHTTPHeader(s http.Header)
+
+	// context related methods
+	SetTimeout(duration time.Duration) (cancel context.CancelFunc)
+	SetDeadline(d time.Time) (cancel context.CancelFunc)
+	Cancel() (cancel context.CancelFunc)
+	NativeContext() context.Context
+
+	// create child goctx
+	WithCancel() (ctx *MapContext, cancel context.CancelFunc)
+	WithDeadline(d time.Time) (ctx *MapContext, cancel context.CancelFunc)
+
+	// golang context() interface
+	Done() <-chan struct{}
+	Err() error
+	Value(key interface{}) interface{}
+	Deadline() (deadline time.Time, ok bool)
+}
+
+// MapContext implements the map concept and mixins the golang context
+type MapContext struct {
+	mu RWMutexInterface // protects following fields
+	context.Context
+
+	m *sync.Map
+}
+
+func Background() Context {
+	return &MapContext{
+		Context: context.Background(),
+		mu:      &sync.RWMutex{},
+		m:       &sync.Map{},
+	}
+}
+
+func TODO() Context {
+	return &MapContext{
+		Context: context.TODO(),
+		mu:      &sync.RWMutex{},
+		m:       &sync.Map{},
+	}
+}
+
+func (c *MapContext) Set(key string, value interface{}) {
+	c.m.Store(key, value)
+}
+
+func (c *MapContext) Get(key string) interface{} {
+	value, _ := c.m.Load(key)
+	return value
+}
+
+func (c *MapContext) Done() <-chan struct{} {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.Context.Done()
+}
+
+func (c *MapContext) GetString(key string) (value string, ok bool) {
+	v := c.Get(key)
+	if v == nil {
+		return
+	}
+	value, ok = v.(string)
+	return
+}
+
+func (c *MapContext) GetStringSlice(key string) (value []string, ok bool) {
+	v := c.Get(key)
+	if v == nil {
+		return
+	}
+	value, ok = v.([]string)
+	return
+}
+
+func (c *MapContext) GetBool(key string) (value, ok bool) {
+	v := c.Get(key)
+	if v == nil {
+		return
+	}
+	value, ok = v.(bool)
+	return
+}
+
+func (c *MapContext) GetInt(key string) (value int, ok bool) {
+	v := c.Get(key)
+	if v == nil {
+		return
+	}
+	value, ok = v.(int)
+	return
+}
+
+func (c *MapContext) GetInt64(key string) (value int64, ok bool) {
+	v := c.Get(key)
+	if v == nil {
+		return
+	}
+	value, ok = v.(int64)
+	return
+}
+
+// Map needs directly call c.Context.Value prevent duplicate lock
+// usecase: copy context, print log get data
+func (c *MapContext) Map() (ret map[string]interface{}) {
+	ret = make(map[string]interface{})
+	c.m.Range(func(k, v interface{}) bool {
+		ret[k.(string)] = v
+		return true
+	})
+	return
+}
+
+// MapString needs directly call c.Context.Value prevent duplicate lock
+func (c *MapContext) MapString() (ret map[string]string) {
+	ret = make(map[string]string)
+	c.m.Range(func(k, v interface{}) bool {
+		switch v.(type) {
+		case string:
+			ret[k.(string)] = v.(string)
+		case []string:
+			ret[k.(string)] = strings.Join(v.([]string), ",")
+		}
+
+		return true
+	})
+
+	//if sp := oteltrace.SpanFromContext(c.Context); sp != nil && sp.SpanContext().TraceID().IsValid() {
+	//	ret[LogKeyTraceID] = sp.SpanContext().TraceID().String()
+	//}
+
+	return
+}
+
+func (c *MapContext) SetTimeout(duration time.Duration) (cancel context.CancelFunc) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.Context, cancel = context.WithTimeout(c.Context, duration)
+	return
+}
+
+func (c *MapContext) SetDeadline(d time.Time) (cancel context.CancelFunc) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.Context, cancel = context.WithDeadline(c.Context, d)
+	return
+}
+
+func (c *MapContext) Cancel() (cancel context.CancelFunc) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.Context, cancel = context.WithCancel(c.Context)
+	return
+}
+
+func (c *MapContext) NativeContext() context.Context {
+	return c.Context
+}
+
+func (c *MapContext) InjectHTTPHeader(rh http.Header) {
+	for hk, sk := range c.HeaderKeyMap() {
+		if s := rh.Get(hk); len(s) == 0 {
+			rh.Set(hk, sk)
+		}
+	}
+}
+
+// HeaderKeyMap returns a map, key is HTTP Header Field, value is the field value stored in Context
+func (c *MapContext) HeaderKeyMap() (ret map[string]string) {
+	ret = make(map[string]string)
+	for sk, hk := range sKMap {
+		v, _ := c.GetString(sk)
+		if len(v) > 0 {
+			ret[hk] = v
+		}
+	}
+
+	//if sp := oteltrace.SpanFromContext(c.Context); sp != nil && sp.SpanContext().TraceID().IsValid() {
+	//	ret[LogKeyTraceID] = sp.SpanContext().TraceID().String()
+	//}
+	//
+	//switch role := c.Get(LogKeyUserRole).(type) {
+	//case string:
+	//	ret[HTTPHeaderUserRole] = role
+	//case []string:
+	//	ret[HTTPHeaderUserRole] = strings.Join(role, ",")
+	//}
+	return
+}
+
+// SetShortenKey sets Context with Header Key, Store in Context with LogKey Key
+func (c *MapContext) SetShortenKey(headerField string, headerValue interface{}) {
+	//if sk, ok := hKMap[headerField]; ok {
+	//	c.Set(sk, headerValue)
+	//}
+}
+
+// GetCID return the CID, if not, generate one
+func (c *MapContext) GetCID() (string, error) {
+	//cid, ok := c.GetString(LogKeyCID)
+	//if !ok {
+	//	cid = strconv.FormatInt(time.Now().UnixNano(), 10)
+	//	c.Set(LogKeyCID, cid)
+	//}
+	return "", nil
+}
+
+func (c *MapContext) Err() error {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.Context.Err()
+}
+
+// Value is the native context.Value() wrap implementation of MapContext.
+// We don't expose any method to set value in native context.
+// We only set tracing span into native context to fit the open source library interface.
+func (c *MapContext) Value(key interface{}) interface{} {
+	return c.Context.Value(key)
+}
+
+func (c *MapContext) Deadline() (deadline time.Time, ok bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.Context.Deadline()
+}
+
+// Note: if parent is not a cancelCtx, then there is no way to cancel children ctx from the parent
+func (parent *MapContext) WithCancel() (ctx *MapContext, cancel context.CancelFunc) {
+	c, cancel := context.WithCancel(parent.Context)
+	ctx = &MapContext{
+		Context: c,
+		m:       copySyncMap(parent.m),
+		mu:      &sync.RWMutex{},
+	}
+	return
+}
+
+// Note: if parent is not a cancelCtx, then there is no way to cancel children ctx from the parent
+func (parent *MapContext) WithDeadline(d time.Time) (ctx *MapContext, cancel context.CancelFunc) {
+	c, cancel := context.WithDeadline(parent.Context, d)
+	ctx = &MapContext{
+		Context: c,
+		m:       copySyncMap(parent.m),
+		mu:      &sync.RWMutex{},
+	}
+	return
+}
+
+func copySyncMap(m *sync.Map) *sync.Map {
+	var cp sync.Map
+
+	m.Range(func(k, v interface{}) bool {
+		vm, ok := v.(sync.Map)
+		if ok {
+			cp.Store(k, copySyncMap(&vm))
+		} else {
+			cp.Store(k, v)
+		}
+
+		return true
+	})
+
+	return &cp
+}
---
+++ ifields.go
@@ -0,0 +1,314 @@
+package gocommont
+
+import (
+	"net/http"
+	"net/textproto"
+	"strings"
+)
+
+const (
+	// ContextKey is the common field if we have to put goctx in other context object
+	ContextKey = "X-Ctx"
+
+	// for trace
+	HTTPHeaderTrace       = "traceparent"
+	HTTPHeaderJaegerDebug = "jaeger-debug-id"
+	HTTPHeaderBaggage     = "baggage"
+
+	// for http passing
+	HTTPHeaderCID            = "x-correlation-id"
+	HTTPHeaderEID            = "x-m800-eid"
+	HTTPHeaderService        = "x-m800-svc"
+	HTTPHeaderClientIP       = "x-forwarded-for"
+	HTTPHeaderClientPort     = "x-forwarded-port"
+	HTTPHeaderClientPlatform = "x-m800-platform"
+	HTTPHeaderDeviceID       = "x-m800-deviceid"
+	HTTPHeaderWebTabID       = "x-m800-tabid"
+
+	HTTPHeaderUserHome       = "x-m800-usr-home"
+	HTTPHeaderServiceHome    = "x-m800-svc-home"
+	HTTPHeaderServiceType    = "x-m800-svc-type"
+	HTTPHeaderServiceRole    = "x-m800-svc-role"
+	HTTPHeaderUserRole       = "x-m800-usr-role"
+	HTTPHeaderUserGroup      = "x-m800-usr-group"
+	HTTPHeaderUserAnms       = "x-m800-usr-anms"
+	HTTPHeaderCustomRole     = "x-m800-custom-role" // This field is for customer-defined role.
+	HTTPHeaderUserDataScope  = "x-m800-usr-data-scope"
+	HTTPHeaderUserDepartment = "x-m800-usr-dept"
+	HTTPHeaderSuperAdmin     = "x-m800-usr-super-admin"
+	HTTPHeaderBotType        = "x-m800-bot-type"
+	HTTPHeaderFromNs         = "x-m800-from-ns"
+	HTTPHeaderInternalCaller = "x-m800-internal-caller"
+	HTTPHeaderSession        = "x-m800-session"
+	HTTPHeaderGroupGuest     = "x-m800-grp-guest"
+	HTTPHeaderGroupRole      = "x-m800-grp-role"
+	HTTPHeaderUserId         = "x-m800-usr-id"
+	HTTPHeaderOmni           = "x-m800-omni" // for aws lambda missing word bug , input x-m800-internal-caller and output this to file-management download api
+
+	// for logger
+	LogKeyTraceID     = "traceID"
+	LogKeyJaegerDebug = "jdi"
+	LogKeyBaggage     = "jb"
+
+	// FROM m800 log format document: https://issuetracking.maaii.com:9443/pages/viewpage.action?pageId=65128541#Loggingformatdesign(v1.0)-Backendteam(BE)
+
+	// LogKeyCID is the cid field key
+	LogKeyCID            = "cid"
+	LogKeyEID            = "eid"
+	LogKeyService        = "svc"
+	LogKeyClientIP       = "clientIP"
+	LogKeyClientPort     = "clientPort"
+	LogKeyClientPlatform = "platform"
+	LogKeyDeviceID       = "deviceID"
+	LogKeyWebTabID       = "webTabID"
+
+	LogKeyUserHome                    = "usrHome"
+	LogKeyServiceHome                 = "svcHome"
+	LogKeyServiceType                 = "svcType"
+	LogKeyUserRole                    = "usrRole"
+	LogKeyUserGroup                   = "usrGroup"
+	LogKeyUserAnms                    = "usrAnms"
+	LogKeyCustomRole                  = "customRole"
+	LogKeyUserDepartment              = "usrDept"
+	LogKeyUserDataScope               = "usrDataScope"
+	LogKeyInternalCaller              = "internalCaller"
+	LogKeyAuditFeatureName            = "feature"
+	LogKeyAuditNeedChangelog          = "changelog"
+	LogKeyRolePermissionAPIMappingKey = "permitKey"
+	LogKeySuperAdmin                  = "superadmin"
+	// LogKeyTimestamp is the time field key
+	LogKeyTimestamp = "time"
+	// LogKeyLevel is the level field key
+	LogKeyLevel = "level"
+	// LogKeyMessage is the message field key
+	LogKeyMessage          = "message"
+	LogKeyEntryType        = "entryType"
+	LogKeyURI              = "uri"
+	LogKeyMethod           = "method"
+	LogKeyCallFunc         = "xfunc"
+	LogKeyCallLine         = "xline"
+	LogKeyHTTPMethod       = "httpMethod"
+	LogKeyInstance         = "instanceID"
+	LogKeyVersion          = "vid"
+	LogKeyCase             = "caseType"
+	LogKeyNamespace        = "ns"
+	LogKeyEnv              = "env"
+	LogKeyErrorCode        = "eCode"
+	LogKeyErrorMessage     = "eMessage"
+	LogKeyErrorType        = "eType"
+	LogKeyWrapErrorCode    = "ueCode"
+	LogKeyWrapErrorMessage = "ueMessage"
+	LogKeyBotType          = "botType"
+	// cross region caller
+	LogKeyFromNamespace = "fromNs"
+
+	// golang tp add fields
+
+	// LogKeyAccessTime is the log field of access time
+	LogKeyAccessTime = "accessTime"
+	// LogKeyLogType is the log type field key
+	LogKeyLogType = "type"
+	// LogKeyApp is the log field of app
+	LogKeyApp = "app"
+)
+
+const (
+	emptyObjectUti = "{}"
+)
+
+// Peeker designed for getting goctx from fasthttp *RequestHeader
+type Peeker interface {
+	Peek(key string) []byte
+}
+
+// Setter designed for setting fasthttp *RequestHeader, net/http Header with goctx
+type Setter interface {
+	Set(key, value string)
+}
+
+// Getter interface designed for getting goctx from net/http Header
+type Getter interface {
+	Get(key string) string
+}
+
+// Deprecated
+// remove for code simplify
+// GetHeaderer interface designed for getting goctx from gin.Context
+type GetHeaderer interface {
+	GetHeader(key string) string
+}
+
+var (
+	sKMap, hKMap map[string]string
+)
+
+func init() {
+	// role special handle
+	sKMap = map[string]string{
+		LogKeyTraceID:        HTTPHeaderTrace,
+		LogKeyJaegerDebug:    HTTPHeaderJaegerDebug,
+		LogKeyBaggage:        HTTPHeaderBaggage,
+		LogKeyCID:            HTTPHeaderCID,
+		LogKeyEID:            HTTPHeaderEID,
+		LogKeyService:        HTTPHeaderService,
+		LogKeyDeviceID:       HTTPHeaderDeviceID,
+		LogKeyWebTabID:       HTTPHeaderWebTabID,
+		LogKeyClientPlatform: HTTPHeaderClientPlatform,
+		LogKeyClientIP:       HTTPHeaderClientIP,
+		LogKeyClientPort:     HTTPHeaderClientPort,
+		LogKeyUserHome:       HTTPHeaderUserHome,
+		LogKeyServiceHome:    HTTPHeaderServiceHome,
+		LogKeyServiceType:    HTTPHeaderServiceType,
+		// LogKeyUserRole:       HTTPHeaderUserRole,
+		LogKeyUserGroup:      HTTPHeaderUserGroup,
+		LogKeyUserAnms:       HTTPHeaderUserAnms,
+		LogKeyCustomRole:     HTTPHeaderCustomRole,
+		LogKeyInternalCaller: HTTPHeaderInternalCaller,
+		LogKeyUserDataScope:  HTTPHeaderUserDataScope,
+		LogKeyUserDepartment: HTTPHeaderUserDepartment,
+		LogKeySuperAdmin:     HTTPHeaderSuperAdmin,
+		LogKeyBotType:        HTTPHeaderBotType,
+		LogKeyFromNamespace:  HTTPHeaderFromNs,
+	}
+
+	hKMap = map[string]string{
+		HTTPHeaderTrace:          LogKeyTraceID,
+		HTTPHeaderJaegerDebug:    LogKeyJaegerDebug,
+		HTTPHeaderBaggage:        LogKeyBaggage,
+		HTTPHeaderCID:            LogKeyCID,
+		HTTPHeaderEID:            LogKeyEID,
+		HTTPHeaderService:        LogKeyService,
+		HTTPHeaderClientIP:       LogKeyClientIP,
+		HTTPHeaderClientPort:     LogKeyClientPort,
+		HTTPHeaderDeviceID:       LogKeyDeviceID,
+		HTTPHeaderWebTabID:       LogKeyWebTabID,
+		HTTPHeaderClientPlatform: LogKeyClientPlatform,
+		HTTPHeaderUserHome:       LogKeyUserHome,
+		HTTPHeaderServiceHome:    LogKeyServiceHome,
+		HTTPHeaderServiceType:    LogKeyServiceType,
+		// HTTPHeaderUserRole:       LogKeyUserRole,
+		HTTPHeaderUserGroup:      LogKeyUserGroup,
+		HTTPHeaderUserAnms:       LogKeyUserAnms,
+		HTTPHeaderCustomRole:     LogKeyCustomRole,
+		HTTPHeaderInternalCaller: LogKeyInternalCaller,
+		HTTPHeaderUserDataScope:  LogKeyUserDataScope,
+		HTTPHeaderUserDepartment: LogKeyUserDepartment,
+		HTTPHeaderSuperAdmin:     LogKeySuperAdmin,
+		HTTPHeaderBotType:        LogKeyBotType,
+		HTTPHeaderFromNs:         LogKeyFromNamespace,
+	}
+}
+
+// IFieldHeaderKeyMap returns a map, key is HTTP Header Field, value is LogKey Field
+func IFieldHeaderKeyMap() (keyMap map[string]string) {
+	return hKMap
+}
+
+// IFieldLogKeyKeyMap returns a map, key is LogKey Field, value is HTTP Header Field
+func IFieldLogKeyKeyMap() (keyMap map[string]string) {
+	return sKMap
+}
+
+func GetContextFromMap(p map[string]interface{}) Context {
+	c := Background()
+	for k, v := range p {
+		c.Set(k, v)
+	}
+	return c
+}
+
+func GetContextFromMapString(p map[string]string) Context {
+	c := Background()
+	for k, v := range p {
+		if k == LogKeyUserRole {
+			c.Set(k, strings.Split(v, ","))
+		} else {
+			c.Set(k, v)
+		}
+	}
+	return c
+}
+
+func GetContextFromHeaderKeyMap(p map[string]string) Context {
+	c := Background()
+	for hk, sk := range hKMap {
+		v := p[hk]
+		if len(v) > 0 {
+			c.Set(sk, v)
+		}
+	}
+	return c
+}
+
+func GetContextFromPeeker(p Peeker) Context {
+	c := Background()
+	var v []byte
+	for hk, sk := range hKMap {
+		v = p.Peek(hk)
+		if len(v) > 0 {
+			c.Set(sk, string(v))
+		}
+	}
+	return c
+}
+
+func GetContextFromGetter(g Getter) Context {
+	c := Background()
+	var v string
+	for hk, sk := range hKMap {
+		v = g.Get(hk)
+		if len(v) > 0 {
+			c.Set(sk, v)
+		}
+	}
+
+	if role := g.Get(HTTPHeaderUserRole); role != "" {
+		c.Set(LogKeyUserRole, strings.Split(role, ","))
+	}
+
+	return c
+}
+
+// Deprecated
+// remove for code simplify
+func GetContextFromGetHeader(g GetHeaderer) Context {
+	c := Background()
+	for hk, sk := range hKMap {
+		v := g.GetHeader(hk)
+		if len(v) > 0 {
+			c.Set(sk, v)
+		}
+	}
+	if role := g.GetHeader(HTTPHeaderUserRole); role != "" {
+		c.Set(LogKeyUserRole, strings.Split(role, ","))
+	}
+
+	return c
+}
+
+func GetContextFromHeader(g http.Header) Context {
+	c := Background()
+	for hk, sk := range hKMap {
+		v := g[textproto.CanonicalMIMEHeaderKey(hk)]
+		if len(v) > 0 {
+			c.Set(sk, strings.Join(v, ","))
+		}
+	}
+	if role := g.Get(HTTPHeaderUserRole); role != "" {
+		c.Set(LogKeyUserRole, strings.Split(role, ","))
+	}
+
+	return c
+}
+
+// CopyContext copy context value and span
+func CopyContext(ctx Context) Context {
+	if ctx == nil {
+		return Background()
+	}
+	newCtx := Background()
+	for k, v := range ctx.Map() {
+		newCtx.Set(k, v)
+	}
+	return newCtx
+}
